81d50c34febf3275e78fe5b49ec706506b099b6b
Atuoha <atuohainitiatives@gmail.com>
2023-01-27 12:44:20 +0100

    refactoring

--- lib/business_logic/cubits/counter_cubit.dart
+++ lib/business_logic/cubits/counter_cubit.dart
@@ -1,16 +1,17 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(const CounterState(counterValue: 0));
+  CounterCubit() : super(CounterState.initial());
 
   void incrementCounter() => emit(
-        CounterState(counterValue: state.counterValue + 1),
+        state.copyWith(counterValue: state.counterValue + 1),
       );
 
   void decrementCounter() => emit(
-        CounterState(counterValue: state.counterValue - 1),
+        state.copyWith(counterValue: state.counterValue + 1),
       );
 }
--- lib/business_logic/cubits/counter_state.dart
+++ lib/business_logic/cubits/counter_state.dart
@@ -1,8 +1,20 @@
 part of 'counter_cubit.dart';
 
 @immutable
-class CounterState {
+class CounterState extends Equatable {
   final int counterValue;
 
   const CounterState({required this.counterValue});
+
+  factory CounterState.initial()=> const CounterState(counterValue: 0);
+
+  @override
+  String toString() => 'CounterState(counterValue: $counterValue)';
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [counterValue];
+
+  CounterState copyWith({int? counterValue}) =>
+      CounterState(counterValue: counterValue ?? this.counterValue);
 }
